60d35f078113945a593283b4662dff30cee732b9
v-song <viviensong2011@gmail.com>
2024-03-26 03:06:30 -0700

    initial engine setup

---
+++ game/src/main.rs
@@ -0,0 +1,91 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+pub struct Engine {
+    assets: AssetCache
+    level: Level,
+    entities: Vec<Entity>,
+    camera: Camera2D,
+    animations: Vec<anim::Animation>,
+    renderer: Immediate,
+}
+impl Engine{
+    pub fn run<G:Game, S>(builder:winit::window::WindowBuilder, cache:AssetCache<S>){
+    let cache = assets_manager::AssetCache::with_source(source);
+
+    let drv = frenderer::Driver::new(
+        winit::window::WindowBuilder::new()
+            .with_title("test")
+            .with_inner_size(winit::dpi::LogicalSize::new(1024.0, 768.0)),
+        Some((W as u32, H as u32)),
+    );
+
+    let mut input = Input::default();
+
+    let mut now = frenderer::clock::Instant::now();
+    let mut acc = 0.0;
+    drv.run_event_loop::<(), _>(
+        move |window, mut frend| {
+            let game = Game::new(&mut frend, &cache);
+            (window, game, frend)
+        },
+        move |event, target, (window, ref mut game, ref mut frend)| {
+            use winit::event::{Event, WindowEvent};
+            match event {
+                Event::WindowEvent {
+                    event: WindowEvent::CloseRequested,
+                    ..
+                } => {
+                    target.exit();
+                }
+                Event::WindowEvent {
+                    event: WindowEvent::Resized(size),
+                    ..
+                } => {
+                    if !frend.gpu.is_web() {
+                        frend.resize_surface(size.width, size.height);
+                    }
+                    window.request_redraw();
+                }
+                Event::WindowEvent {
+                    event: WindowEvent::RedrawRequested,
+                    ..
+                } => {
+                    let elapsed = now.elapsed().as_secs_f32();
+                    // You can add the time snapping/death spiral prevention stuff here if you want.
+                    // I'm not using it here to keep the starter code small.
+                    acc += elapsed;
+                    now = std::time::Instant::now();
+                    // While we have time to spend
+                    while acc >= DT {
+                        // simulate a frame
+                        acc -= DT;
+                        game.simulate(&input, DT);
+                        input.next_frame();
+                    }
+                    game.render(frend);
+                    frend.render();
+                    window.request_redraw();
+                }
+                event => {
+                    input.process_input_event(&event);
+                }
+            }
+        },
+    )
+    .expect("event loop error");
+}
+    }
+}
+trait Game{
+    fn new (engine: &mut Engine) -> Self;
+    fn update (&mut self, engine: &mut Engine, dt: f32);
+}
+
+struct Entity {
+    pos: Vec2, 
+    vel: Vec2, 
+    acc: Vec2, 
+    anim: anim::AnimState,
+}
